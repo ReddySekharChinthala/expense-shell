22e71aec7ef9cdf26c5973b4bc06a10fecb74367
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-12-01 16:46:48 +0530

    backend-service

---
+++ backend.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "please enter the password:"
+read -s mysql_root_password
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE. $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS. $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run the script with root access."
+    exit 1
+else 
+    echo "You are super user."
+fi
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+id expense
+if [ $? -ne 0 ]
+then
+    useradd expense
+    VALIDATE $? "Creating expense user"
+else
+    echo "User already exist..$Y SKIPPING $N"
+fi
+
