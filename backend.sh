feb57e824747c0ab8e6c66b78b0f4a080091b986
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-12-01 17:02:46 +0530

    backend-service

--- backend.sh
+++ backend.sh
@@ -49,6 +49,13 @@ else
 fi
 
 # below -p is to check whether exist app directory is there are not if its there it will ignore, otherwise it will give error.
-mkdir -p /app
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
+
+cd /app
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
 
